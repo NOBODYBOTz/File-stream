f0da6f5c3792d0344a9311380108c79dab6dc1ec
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-07-24 12:05:30 +0530

    Add files via upload

---
+++ nobody.py
@@ -0,0 +1,17 @@
+import os
+import pytz
+import time, re
+from datetime import datetime
+
+
+def get_wish():
+    tz = pytz.timezone('Asia/Colombo')
+    time = datetime.now(tz)
+    now = time.strftime("%H")
+    if now < "12":
+        status = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 🌞"
+    elif now < "18":
+        status = "ɢᴏᴏᴅ ᴀꜰᴛᴇʀɴᴏᴏɴ 🌗"
+    else:
+        status = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌘"
+    return status
