b89e025f4d229d9bc4cbe24c41ac75e10f10e91d
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-07-24 12:06:15 +0530

    Update commands.py

--- biisal/bot/plugins/commands.py
+++ biisal/bot/plugins/commands.py
@@ -14,6 +14,7 @@ from biisal.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
 from biisal.vars import bot_name , bisal_channel , bisal_grp
+from nobody import get_wish
 
 
 SRT_TXT = """<b>ᴊᴀɪ sʜʀᴇᴇ ᴋʀsɴᴀ {}!,
