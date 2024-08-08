6aea54021a35abff08da49623487c2b4e0da6727
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-08-08 16:58:03 +0530

    Update nobody.py

--- nobody.py
+++ nobody.py
@@ -3,25 +3,6 @@ import pytz
 import time, re
 from datetime import datetime
 
-var myEmoji = ["👍", "❤", "🔥", "🥰", "😁", "🎉", "🤩", "🕊", "🥱","😍", "🐳", "❤‍🔥", "🌭", "💯", "⚡", "😇", "🤝", "✍", "🤗", "🎅", "🎄", "☃", "💘", "🦄", "😘", "😎"]
-
-var doEmoji = myEmoji[Math.floor(Math.random() * myEmoji.length)];
-
-HTTP.post({
-  url: "https://api.telegram.org/bot" + bot.token + "/setMessageReaction",
-  body: {
-    chat_id: request.chat.id,
-    message_id: request.message_id,
-    reaction: JSON.stringify([
-      {
-        type: "emoji",
-        emoji: doEmoji,
-        is_big: true 
-      }
-    ])
-  }
-});
-
 def get_wish():
     tz = pytz.timezone('Asia/Colombo')
     time = datetime.now(tz)
