939519a066d8b91d0439f27f202fb840ea4e8c8d
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-08-14 19:08:20 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -7,15 +7,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
-bisal_channel = "https://telegram.me/bisal_files"
-bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
+bisal_channel = "https://telegram.me/NOBODY_BOTz"
+bisal_grp = "https://t.me/NOBODY_BOTz_support"
 
 class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '25529076'))
     API_HASH = str(getenv('API_HASH', 'b8109a26ed1ba595aa5d70d0e38c3806'))
     BOT_TOKEN = str(getenv('BOT_TOKEN' , '6716430878:AAEuvuYbwv2tlR1cUzvS21ia5L-v_TIgmdk'))
-    name = str(getenv('name', 'bisal_file2link_bot'))
+    name = str(getenv('name', 'FILE_TO_LINK_01_BOT'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     RESTAT = int(getenv('RESTAT', '-1001955617973'))
@@ -27,7 +27,7 @@ class Var(object):
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "1715861188").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'avinash_7_3_3'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NOBODY_BOTz'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
@@ -41,7 +41,7 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://avinashkakde646:avinashkakde646@cluster0.jlubkn6.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'itsak733')) 
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'NOBODY_BOTz')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @biisal_bot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
